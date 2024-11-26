65c14f9294d6134cfb95ed334047b29354648b29
Aleksa-merike <zap1271@amerike.edu.mx>
2024-11-26 14:01:50 -0600

    Add files via upload

---
+++ P002_Valor_Absoluto/absoluto.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<math.h>
+using namespace::std;
+main()
+{
+    float numero;
+    cout<<"-----Valor absoluto-----"<<endl<<"Que numero buscas?: ";
+    
+    cin>>numero;
+
+    cout<<"El valor absoluto es: "<<abs(numero)<<endl;
+}
